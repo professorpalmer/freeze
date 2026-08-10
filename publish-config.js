7a32b54e485a8293340a8ed4f4281c4d08135277
Cary Palmer <carypalmer@mac.lan>
2026-08-10 04:05:28 -0500

    chore: refresh overnight publish tunnel URL

--- publish-config.js
+++ publish-config.js
@@ -2,7 +2,7 @@
 window.FREESE_PUBLISH = {
   passphrase: 'traditionology',
   endpoints: [
-    'https://f6c8763eab9562.lhr.life/api/publish',
+    'https://6619ab34e6d60b.lhr.life/api/publish',
     'http://127.0.0.1:8787/api/publish'
   ]
 };
